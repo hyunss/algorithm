c24a241ff538b6991a545f1cc0dfd02ee55e8e43
hyunss <khh3599@naver.com>
2021-01-13 17:41:05 +0900

    baekjoon_11650 solve

--- algorithm_variety/src/algorithm_variety/algorithm_11650.java
+++ algorithm_variety/src/algorithm_variety/algorithm_11650.java
@@ -3,15 +3,38 @@ package algorithm_variety;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class algorithm_11650 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(bf.readLine());
-		for (int i = 0; i < args.length; i++) {
+		StringBuilder sb = new StringBuilder();
+		
+		int[][] input = new int[num][2];
+		
+		
+		for (int i = 0; i < num; i++) {
+			String xy = bf.readLine();
+			StringTokenizer st = new StringTokenizer(xy, " ");
+			input[i][0] = Integer.parseInt(st.nextToken());
+			input[i][1] = Integer.parseInt(st.nextToken());
 			
 		}
+		
+		Arrays.sort(input, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		for(int i=0; i<num; i++) {
+			sb.append(input[i][0] + " " + input[i][1]).append("\n");
+		}
+		System.out.println(sb);
 	}
 
 }
