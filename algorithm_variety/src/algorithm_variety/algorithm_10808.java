1fb7d444879170b4eff05d3ebf775ea73e6878a2
hyunss <khh3599@naver.com>
2021-04-13 17:13:23 +0900

    algorithm_10808

---
+++ algorithm_variety/src/algorithm_variety/algorithm_10808.java
@@ -0,0 +1,25 @@
+package algorithm_variety;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class algorithm_10808 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String input = bf.readLine();
+		char[] alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		int[] count = new int[26];
+		for(int i=0; i<input.length(); i++) {
+			for(int j=0; j<alpha.length; j++) {
+				if(input.charAt(i) == alpha[j]) {
+					count[j]++;
+				}
+			}
+			
+		}
+		for(int i=0; i<count.length;i++) {
+			System.out.print(count[i] + " ");
+		}
+	}
+}
