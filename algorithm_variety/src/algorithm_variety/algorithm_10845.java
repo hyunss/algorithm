0c082e96536e43bee86abcb1962e54950f2415cc
hyunss <khh3599@naver.com>
2021-04-13 16:53:10 +0900

    algorithm_10866

--- algorithm_variety/src/algorithm_variety/algorithm_10845.java
+++ algorithm_variety/src/algorithm_variety/algorithm_10845.java
@@ -10,45 +10,62 @@ public class algorithm_10845 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(bf.readLine());
-		ArrayList<Integer> queue =  new ArrayList<>();
+		ArrayList<Integer> deque =  new ArrayList<>();
 		
 		for(int i=0; i<num; i++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			String input = st.nextToken();
 			
-			if(input.equals("push")) {
-				queue.add(Integer.parseInt(st.nextToken()));
+			if(input.equals("push_front")) {
+				deque.add(0, Integer.parseInt(st.nextToken()));
 			}
-			else if(input.equals("front")) {
-				if(queue.size() == 0 ){
-					System.out.println(-1);
-				}
-				else System.out.println(queue.get(0));
+			else if(input.equals("push_back")) {
+				deque.add(Integer.parseInt(st.nextToken()));
 			}
 			else if(input.equals("size")) {
-				System.out.println(queue.size());
+				System.out.println(deque.size());
 			}
 			else if(input.equals("empty")) {
-				if(queue.size() == 0) {
+				if(deque.size() == 0) {
 					System.out.println(1);
 				}
 				else System.out.println(0);
 			}
-			else if(input.equals("pop")) {
-				if(queue.size() == 0) {
+			else if(input.equals("pop_front")) {
+				if(deque.size() == 0 ){
 					System.out.println(-1);
 				}
 				else {
-					int popNum = queue.get(0);
-					queue.remove(0);
+					int popNum = deque.get(0);
+					deque.remove(0);
 					System.out.println(popNum);
 				}
 			}
-			else {
-				if(queue.size() == 0){
+			else if(input.equals("pop_back")) {
+				if(deque.size() == 0 ){
 					System.out.println(-1);
 				}
-				else System.out.println(queue.get(queue.size()-1));
+				else {
+					int popNum = deque.get(deque.size()-1);
+					deque.remove(deque.size()-1);
+					System.out.println(popNum);
+				}
+			}
+			else if(input.equals("front")) {
+				if(deque.size() == 0 ){
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(deque.get(0));
+				}
+			}
+			else if(input.equals("back")) {
+				if(deque.size() == 0 ){
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(deque.get(deque.size()-1));
+				}
 			}
 		}
 	}
