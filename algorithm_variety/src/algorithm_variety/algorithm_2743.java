bdfbef6073dbe7b967fa90b479ddb57666925bae
hyunss <khh3599@naver.com>
2021-04-14 15:32:15 +0900

    algorithm_2743

--- algorithm_variety/src/algorithm_variety/algorithm_2743.java
+++ algorithm_variety/src/algorithm_variety/algorithm_2743.java
@@ -1,5 +1,13 @@
 package algorithm_variety;
 
-public class algorithm_2743 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class algorithm_2743 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String input = bf.readLine();
+		System.out.println(input.length());
+	}
 }
