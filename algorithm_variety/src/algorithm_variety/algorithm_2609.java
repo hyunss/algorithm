9065e4a3494514ebc95119a07c57bccac9e65434
hyunss <khh3599@naver.com>
2021-04-16 19:28:29 +0900

    algorithm_2609

--- algorithm_variety/src/algorithm_variety/algorithm_2609.java
+++ algorithm_variety/src/algorithm_variety/algorithm_2609.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class algorithm_2609 {
 	 public static void main(String[] args) throws Exception {
 	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	        StringTokenizer st = new StringTokenizer(br.readLine());
 	        
 	        int a = Integer.parseInt(st.nextToken());
