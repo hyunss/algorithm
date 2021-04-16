589f50ea1fc1a712e8a848f3a2663629019b59f3
hyunss <khh3599@naver.com>
2021-04-16 19:25:45 +0900

    algorithm_2609

---
+++ algorithm_variety/src/algorithm_variety/algorithm_2609.java
@@ -0,0 +1,35 @@
+package algorithm_variety;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class algorithm_2609 {
+	 public static void main(String[] args) throws Exception {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+	        
+	        int a = Integer.parseInt(st.nextToken());
+	        int b = Integer.parseInt(st.nextToken());
+	       
+	        int gcd = getGCD(Math.max(a, b), Math.min(a, b));
+	        int lcm = getLCM(a, b, gcd);
+	        
+	        System.out.println(gcd);
+	        System.out.println();
+	 }
+	 public static int getGCD(int a, int b) {
+		 while(b>0) {
+			 int temp = a;
+			 a = b;
+			 b = temp % b;
+		 }
+		 return a;
+	 }
+	 public static int getLCM(int a, int b, int gcd) {
+		 return (a*b)/gcd;
+	 }
+}
